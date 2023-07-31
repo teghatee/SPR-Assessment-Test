17c502f18a63f755e618f8e8441effc6f3ebd352
ayuba pelumi <132446974+adebart@users.noreply.github.com>
2023-07-31 23:10:28 +0100

    my first comit

---
+++ main.js
@@ -0,0 +1,308 @@
+var tiim ;
+var tin ;
+var dey = function swwww(){
+    console.log(ques3)
+    document.getElementById("woti").innerHTML = "You scored " + ques3 + " / 10"
+}
+function start(){
+    
+    tiim = setInterval(function(){document.getElementById('ww').innerHTML = document.getElementById('ww').innerHTML -1;
+
+        if(document.getElementById('ww').innerHTML == -1){
+            clearInterval(tiim)
+            alert("Your time is up")
+            document.getElementById("koko").style.display = "none"
+            document.getElementById("lolo").style.display = "none"
+            document.getElementById("ret").style.display = "none"
+            document.getElementById("ccc").style.display = "none"
+            document.getElementById("hol").style.display = "none"
+            document.getElementById("ggg").style.display = "none"
+            document.getElementById("frrr").style.display = "none"
+            document.getElementById("weq").style.display = "none"
+            document.getElementById("wee").style.display = "none"
+            document.getElementById("bhb").style.display = "none"
+            document.getElementById('ww').innerHTML = 0
+            clearInterval(tiim)
+            
+            
+                
+            
+        }
+    },60000)
+    tin = setInterval(function(){document.getElementById('wq').innerHTML = document.getElementById('wq').innerHTML -1;
+
+        if(document.getElementById('wq').innerHTML == 0){
+            
+            document.getElementById('wq').innerHTML =60
+            clearInterval(tin)
+
+            
+            
+        }
+        if(document.getElementById('ww').innerHTML == -1){
+    
+            clearInterval(tin)
+            document.getElementById('ww').innerHTML = 60
+            clearInterval(tin)
+            clearInterval(tin)
+        
+        
+            
+        }
+    },1000)
+    document.getElementById("pop").style.display = "block"
+    document.getElementById("weew").style.display = "none"
+}
+// var number = 0
+// parseInt(number)
+// function slect(){
+//     // number = number + 1
+//     // number = parseInt(number) + 1
+//     document.createElement("p").innerHTML.appendChild(document.createElement("p").innerHTML + 1)
+    
+// }
+// function slect(){
+//     number = +0
+// }
+
+// var nor = document.createElement("p").innerHTML
+// var gey = document.createTextNode(1)
+// // nor.appendChild(gey)
+// document.createElement("p").innerHTML.appendChild(document.createTextNode(1))
+var ques3 = 0;
+
+function two(){
+    ques3 = parseInt(ques3) + 1
+    console.log(ques3)
+    if(ques3 => 1){
+        // document.getElementById("swu").innerHTML = "correct" 
+    }
+    // document.getElementById('aqq').style.backgroundColor = "white"
+
+}
+function one(){
+    ques3 = parseInt(ques3) + 0
+    console.log(ques3)
+    if(ques3 <= 0){
+        // document.getElementById("swu").innerHTML = "wrong" 
+        
+    }
+    
+}
+function swwww(){
+    console.log(ques3)
+    document.getElementById("woti").innerHTML = "You scored " + ques3 + " / 10"
+}
+// mm
+var ques3 = 0;
+
+function twwo(){
+    ques3 = parseInt(ques3) + 1
+    console.log(ques3)
+    if(ques3 => 1){
+        // document.getElementById("swuu").innerHTML = "correct" 
+    }
+
+}
+function oone(){
+    ques3 = parseInt(ques3) + 0
+    console.log(ques3)
+    if(ques3 <= 0){
+        // document.getElementById("swuu").innerHTML = "wrong" 
+    }
+
+}
+// kkk
+var ques3 = 0;
+
+function twowo(){
+    ques3 = parseInt(ques3) + 1
+    console.log(ques3)
+    if(ques3 => 1){
+        // document.getElementById("swuu").innerHTML = "correct" 
+    }
+
+}
+function oowne(){
+    ques3 = parseInt(ques3) + 0
+    console.log(ques3)
+    if(ques3 <= 0){
+        // document.getElementById("swuu").innerHTML = "wrong" 
+    }
+
+}
+
+// sfghjkhgf
+
+var ques3 = 0;
+
+function twwo(){
+    ques3 = parseInt(ques3) + 1
+    console.log(ques3)
+    if(ques3 => 1){
+        // document.getElementById("seeq").innerHTML = "correct" 
+    }
+
+}
+function oone(){
+    ques3 = parseInt(ques3) + 0
+    console.log(ques3)
+    if(ques3 <= 0){
+        // document.getElementById("seeq").innerHTML = "wrong" 
+    }
+
+}
+// ghjk
+
+
+var ques3 = 0;
+
+function twwo(){
+    ques3 = parseInt(ques3) + 1
+    console.log(ques3)
+    if(ques3 => 1){
+        // document.getElementById("swuu").innerHTML = "correct" 
+    }
+
+}
+function oone(){
+    ques3 = parseInt(ques3) + 0
+    console.log(ques3)
+    if(ques3 <= 0){
+        // document.getElementById("swuu").innerHTML = "wrong" 
+    }
+
+}
+function dtt(){
+    document.getElementById("pop").style.display = "none"
+    document.getElementById("lolo").style.display = "block"
+    document.getElementById("weew").style.display = "block"
+}
+function dttt(){
+    document.getElementById("pop").style.display = "block"
+    document.getElementById("lolo").style.display = "none"
+}
+function dot(){
+    document.getElementById("ret").style.display = "block"
+    document.getElementById("lolo").style.display = "none"
+}
+function dott(){
+    document.getElementById("ret").style.display = "none"
+    document.getElementById("lolo").style.display = "block"
+}
+function doot(){
+    document.getElementById("ccc").style.display = "block"
+    document.getElementById("ret").style.display = "none"
+}
+function dtto(){
+    document.getElementById("ccc").style.display = "none"
+    document.getElementById("ret").style.display = "block"
+}
+function ctto(){
+    document.getElementById("wee").style.display = "block"
+    document.getElementById("ccc").style.display = "none"
+}
+function coot(){
+    document.getElementById("wee").style.display = "none"
+    document.getElementById("ccc").style.display = "block"
+}
+function coott(){
+    document.getElementById("wee").style.display = "none"
+    document.getElementById("hol").style.display = "block"
+}
+function just(){
+    document.getElementById("hol").style.display = "none"
+    document.getElementById("wee").style.display = "block"
+}
+function jdtt(){
+    document.getElementById("hol").style.display = "none"
+    document.getElementById("ggg").style.display = "block"
+}
+function dtott(){
+    document.getElementById("ggg").style.display = "none"
+    document.getElementById("hol").style.display = "block"
+}
+function ddvtt(){
+    document.getElementById("ggg").style.display = "none"
+    document.getElementById("frrr").style.display = "block"
+}
+function dttoot(){
+    document.getElementById("frrr").style.display = "none"
+    document.getElementById("ggg").style.display = "block"
+}
+function dctt(){
+    document.getElementById("frrr").style.display = "none"
+    document.getElementById("weq").style.display = "block"
+}
+function dcwtt(){
+    document.getElementById("weq").style.display = "none"
+    document.getElementById("frrr").style.display = "block"
+}
+function dttp(){
+    document.getElementById("weq").style.display = "none"
+    document.getElementById("bhb").style.display = "block"
+    document.getElementById("weew").style.display = "block"
+}
+function dtptt(){
+    document.getElementById("bhb").style.display = "none"
+    document.getElementById("weq").style.display = "block"
+}
+// function quiz(){
+    
+//     // document.getElementsById("koko").style.display = "none"
+//     document.getElementById("pop").style.display = "block"
+//     document.getElementById("weew").style.display = "none"
+// }
+
+function quiz(){
+    
+    tiim = setInterval(function(){document.getElementById('ww').innerHTML = document.getElementById('ww').innerHTML -1;
+
+        if(document.getElementById('ww').innerHTML == -1){
+            clearInterval(tiim)
+            alert("Your time is up")
+            document.getElementById("koko").style.display = "none"
+            document.getElementById("lolo").style.display = "none"
+            document.getElementById("ret").style.display = "none"
+            document.getElementById("ccc").style.display = "none"
+            document.getElementById("hol").style.display = "none"
+            document.getElementById("ggg").style.display = "none"
+            document.getElementById("frrr").style.display = "none"
+            document.getElementById("weq").style.display = "none"
+            document.getElementById("wee").style.display = "none"
+            document.getElementById("bhb").style.display = "none"
+            document.getElementById('ww').innerHTML = 0
+            clearInterval(tiim)
+            
+            
+                
+            
+        }
+    },60000)
+    tin = setInterval(function(){document.getElementById('wq').innerHTML = document.getElementById('wq').innerHTML -1;
+
+        if(document.getElementById('wq').innerHTML == 0){
+            
+            document.getElementById('wq').innerHTML =60
+            clearInterval(tin)
+
+            
+            
+        }
+        if(document.getElementById('ww').innerHTML == -1){
+    
+            clearInterval(tin)
+            document.getElementById('ww').innerHTML = 60
+            clearInterval(tin)
+            clearInterval(tin)
+        
+        
+            
+        }
+    },1000)
+    document.getElementById("pop").style.display = "block"
+    document.getElementById("weew").style.display = "none"
+}
+
+
+
